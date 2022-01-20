a5dbd74b125d82a39f992122263875c8220b16e8
akifsonmez <akifsonmez@outlook.com>
2022-01-20 23:27:46 +0300

    Add useState examples

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [user, setUser] = useState({ name: "Akif", age: 30 });
   return (
     <div className="App">
-      React Hooks Tutorial
+      {counter}
+      <br />
+      <button
+        onClick={() => {
+          // incorrect way to update state
+          const newCount = counter + 1;
+          setCounter(newCount);
+        }}
+      >
+        +
+      </button>
+      <br />
+      <button
+        onClick={() => {
+          // correct way to update state
+          setCounter((prevCount) => prevCount - 1);
+        }}
+      >
+        -
+      </button>
+      <br />
+      {user.name} is {user.age} years old.
+      <br />
+      <button
+        onClick={() =>
+          setUser((prevUser) => {
+            return { ...prevUser, age: 25 };
+          })
+        }
+      >
+        Correct the age
+      </button>
     </div>
   );
 }
