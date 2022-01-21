4961cd7671d6f417f3b5feb60009d6a7e9768995
akifsonmez <akifsonmez@outlook.com>
2022-01-21 19:59:45 +0300

    Add useContext examples

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext, useContext } from "react";
+
+const ChildColorContext = createContext();
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [childColor, setChildColor] = useState("red")
   const [user, setUser] = useState({ name: "Akif", age: 30 });
 
   useEffect(() => {
     console.log("first mount and every render");
     return () => {
-      console.log("every onmount")
-    }
+      console.log("every onmount");
+    };
   });
   useEffect(() => {
     console.log("first mount and every counter change");
     return () => {
-      console.log("after counter change onmount")
-    }
+      console.log("after counter change onmount");
+    };
   }, [counter]);
 
   return (
@@ -52,8 +55,23 @@ function App() {
       >
         Correct the age
       </button>
+      <ChildColorContext.Provider value={childColor}>
+        <ChildComponent />
+        <AnotherChildComponent />
+      </ChildColorContext.Provider>
+      <button onClick={() => setChildColor(prevColor => prevColor === "red" ? "blue" : "red")}>Switch color</button>
     </div>
   );
 }
+
+const ChildComponent = () => {
+  const color = useContext(ChildColorContext);
+  return <h1 style={{ color }}>This is a child component</h1>;
+};
+
+const AnotherChildComponent = () => {
+  const color = useContext(ChildColorContext);
+  return <h2 style={{ color }}>Another child component</h2>;
+};
 
 export default App;
